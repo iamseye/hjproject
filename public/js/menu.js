fd8ca323b5a8defa6e23d98d484d6a07662d85c3
iamseye <Jessie@Jessiede-MacBook-Air.local>
2016-09-01 17:18:29 +0800

    menu.js

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,28 +1,40 @@
 function menu(){
+
+    var pgurl = window.location.href;
+
+    $("#mainMenu a").each(function(){
+        var p = $(this).attr("href");
+
+        if( p == pgurl ){
+            $(this).children('li').addClass("this");
+        }
+    });
+
     for (var i = 0; i < $("#mainMenu li").size(); i++ )
     {
         var $this = $("#mainMenu a").eq(i);
-        $this.children("li").css("background-image","url('/images/menu_0"+i+"_c.png')");
-        $this.children("li.this").css("background-image","url('/images/menu_0"+i+"_h.png')");
+        $this.children("li").css("background-image","url('./images/menu_0"+i+"_c.png')");
+        $this.children("li.this").css("background-image","url('./images/menu_0"+i+"_h.png')");
         $this.children("li").css("background-size","cover");
 
         preload = new Image();
-        preload.src = "/images/menu_0"+i+"_h.png";
+        preload.src = "./images/menu_0"+i+"_h.png";
     }
 
     $("#mainMenu a").hover(function(){
         var s = $("#mainMenu a").index(this),
             $this = $(this);
 
-        $this.children("li").css("background-image","url('/images/menu_0"+s+"_h.png')");
+        $this.children("li").css("background-image","url('./images/menu_0"+s+"_h.png')");
     },function(){
         var s = $("#mainMenu a").index(this),
             $this = $(this);
 
-        $this.children("li").css("background-image","url('/images/menu_0"+s+"_c.png')");
-        $this.children("li.this").css("background-image","url('/images/menu_0"+s+"_h.png')");
+        $this.children("li").css("background-image","url('./images/menu_0"+s+"_c.png')");
+        $this.children("li.this").css("background-image","url('./images/menu_0"+s+"_h.png')");
     });
 }
+
 
 $(document).ready(function(){
 
